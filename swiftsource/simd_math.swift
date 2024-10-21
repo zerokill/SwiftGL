df02f174570f2d82a1c6891cbc5b2b97e7af4f20
Maurice Daverveldt <maurice.daverveldt@gmail.com>
2024-10-21 21:05:52 +0200

    Cleanup

--- swiftsource/simd_math.swift
+++ swiftsource/simd_math.swift
@@ -31,6 +31,19 @@ extension float4x4 {
         return matrix
     }
 
+    static func lookAt(eye: SIMD3<Float>, center: SIMD3<Float>, up: SIMD3<Float>) -> float4x4 {
+        let f = normalize(center - eye)
+        let s = normalize(cross(f, up))
+        let u = cross(s, f)
+
+        var result = matrix_identity_float4x4
+        result.columns.0 = SIMD4<Float>(s.x, u.x, -f.x, 0)
+        result.columns.1 = SIMD4<Float>(s.y, u.y, -f.y, 0)
+        result.columns.2 = SIMD4<Float>(s.z, u.z, -f.z, 0)
+        result.columns.3 = SIMD4<Float>(-dot(s, eye), -dot(u, eye), dot(f, eye), 1)
+        return result
+    }
+
     // Perspective projection matrix
     static func perspective(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> float4x4 {
         let ys = 1 / tan(fovy * 0.5)
