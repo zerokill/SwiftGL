faf1eb80ba6a7157ab2591b522fb1decde04d191
Maurice Daverveldt <maurice.daverveldt@gmail.com>
2025-10-30 07:43:01 +0100

    cleanup

--- swiftsource/leonMesh.swift
+++ swiftsource/leonMesh.swift
@@ -25,23 +25,23 @@ class LeonMesh: Mesh {
         let latitudeBands = parameters.latitudeBands
         let longitudeBands = parameters.longitudeBands
         let radius = parameters.radius
-        
+
         for lat in 0...latitudeBands {
             let theta = Float(lat) * Float.pi / Float(latitudeBands)
             let sinTheta = sin(theta)
             let cosTheta = cos(theta)
-            
+
             for lon in 0...longitudeBands {
                 let phi = Float(lon) * 2.0 * Float.pi / Float(longitudeBands)
                 let sinPhi = sin(phi)
                 let cosPhi = cos(phi)
-                
+
                 let x = cosPhi * sinTheta
                 let y = cosTheta
                 let z = sinPhi * sinTheta
                 let u = 1.0 - (Float(lon) / Float(longitudeBands))
                 let v = 1.0 - (Float(lat) / Float(latitudeBands))
-                
+
                 let vertex = Vertex(
                     position: [x * radius, y * radius, z * radius],
                     normal: [x, y, z],
@@ -50,22 +50,22 @@ class LeonMesh: Mesh {
                 vertices.append(vertex)
             }
         }
-        
+
         for lat in 0..<latitudeBands {
             for lon in 0..<longitudeBands {
                 let first = (lat * (longitudeBands + 1)) + lon
                 let second = first + longitudeBands + 1
-                
+
                 indices.append(GLuint(first))
                 indices.append(GLuint(first + 1))
                 indices.append(GLuint(second))
-                
+
                 indices.append(GLuint(second))
                 indices.append(GLuint(first + 1))
                 indices.append(GLuint(second + 1))
             }
         }
-        
+
         return (vertices, indices)
     }
 
